4dfbec70573970da7b24fdc25f54eea05fe29743
Flavio Cabrera <43635726+Shellcode-KR@users.noreply.github.com>
2026-04-02 14:41:31 -0600

    Agregar interfaz gráfica y refactor de motor Sudoku

---
+++ src/main/java/com/flavio/Coordenada.java
@@ -0,0 +1,43 @@
+package com.flavio;
+
+import java.util.Objects;
+
+public class Coordenada {
+    private final int fila;
+    private final int columna;
+
+    public Coordenada(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Coordenada that = (Coordenada) obj;
+        return fila == that.fila && columna == that.columna;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fila, columna);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + (fila + 1) + ", " + (columna + 1) + ")";
+    }
+}
--- src/main/java/com/flavio/Main.java
+++ src/main/java/com/flavio/Main.java
@@ -2,11 +2,6 @@ package com.flavio;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        TableroSudoku tablero = new TableroSudoku();
-        Vista vista = new Vista();
-
-        vista.imprimirCuadricula(tablero.getCuadricula());
+        Vista.iniciar();
     }
 }
